ab406f5e0fc87b99d8eaa43704efb6097ca164a7
Niihad <yougo54@hotmail.fr>
2017-05-15 17:04:38 +0200

    Lesson 5 : ajout methode ombrage de Gouraud

--- Infographie.cpp
+++ Infographie.cpp
@@ -31,6 +31,8 @@ const TGAColor yellow = TGAColor(255, 255, 0, 255);
 Model *model = NULL;
 const int width  = 800;
 const int height = 800;
+FloatElement focus(1,-1,1);
+
 
 Matrix viewport(int x, int y, int w, int h) {
 	Matrix view(4,4);
@@ -82,7 +84,7 @@ bool barycentric(float aire_e, FloatElement *e, FloatElement &p, float &alpha, f
     return ((fabs(res - 1) < 0.01)) ? true : false;
 }
 
-void triangle(FloatElement *e, FloatElement *e_text, float **z_buffer, TGAImage &image, TGAImage &texture, float intensity) {
+void triangle(FloatElement *e, FloatElement *e_texture, float *f_intensity, float **z_buffer, TGAImage &image, TGAImage &texture) {
     FloatElement bbmin(std::max(0.f,std::min((float)image.get_width()-1,std::min(e[0].x,std::min(e[1].x,e[2].x)))),
                      std::max(0.f,std::min((float)image.get_height()-1,std::min(e[0].y,std::min(e[1].y,e[2].y)))));
     FloatElement bbmax(std::min((float)image.get_width()-1,std::max(e[0].x,std::max(e[1].x,e[2].x))),
@@ -90,21 +92,37 @@ void triangle(FloatElement *e, FloatElement *e_text, float **z_buffer, TGAImage 
 
     float aire_e = aire(e[0], e[1], e[2]);
     float alpha,beta,gamma;
-    float txtX, txtY;
+    float txtX, txtY, ity;
     for(int i=bbmin.x; i<=bbmax.x;i++){
         for(int j=bbmin.y; j<=bbmax.y;j++){
             FloatElement tmp(i,j);
             if(barycentric(aire_e, e, tmp,alpha,beta,gamma)){
                 if(z_buffer[i][j] < tmp.z){
                     z_buffer[i][j] = tmp.z;
-                    txtX = (e_text[0].x*alpha + e_text[1].x*beta + e_text[2].x*gamma)*texture.get_width();
-                    txtY = (e_text[0].y*alpha + e_text[1].y*beta + e_text[2].y*gamma)*texture.get_height();
-                    TGAColor color = texture.get(txtX,txtY);
-                    image.set(i, j, TGAColor(color.bgra[2]*intensity, color.bgra[1]*intensity, color.bgra[0]*intensity));
+                    ity = (f_intensity[0]*alpha + f_intensity[1]*beta + f_intensity[2]*gamma)*texture.get_width();
+                    image.set(i, j, TGAColor(255, 255, 255)*ity);
                 }
             }
         }
     }
+}
+
+Matrix perspective(){
+    Matrix perse(4,4);
+    perse(0,0) = perse(1,1) = perse(2,2) = perse(3,3) = 1;
+    perse(3,2) = -1.f/3;
+    return perse;
+}
+
+/* Technique de ombrage plat pour calculer la luminosite d une face*/
+float ombragePlat(FloatElement *current){
+    /* calcule des vecteurs ab et ac */
+    FloatElement ab = (current[1]-current[0]);
+    FloatElement ac = (current[2]-current[0]);
+    /* calcule du produit vectoriel de ab^ac */
+    FloatElement pv = (ac)^(ab);
+    pv.normaliser();
+    return (pv.x*focus.x + pv.y*focus.y + pv.z*focus.z);
 }
 
 int main(int argc, char** argv) {
@@ -117,7 +135,7 @@ int main(int argc, char** argv) {
 
     TGAImage image(width, height, TGAImage::RGB);
     Matrix view = viewport(width/8, height/8, width*3/4, height*3/4);
-    FloatElement focus(0,0,-1);
+
     float **z_buffer = new float*[width];
     for (int i=0; i<width; i++)
         z_buffer[i] = new float[height];
@@ -126,11 +144,12 @@ int main(int argc, char** argv) {
             z_buffer[i][j] = -std::numeric_limits<float>::max();
         }
     }
+    focus.normaliser();
 
+    /* gestion de la perspective */
     Matrix tmp(4,1);
-    Matrix perse(4,4);
-    perse(0,0) = perse(1,1) = perse(2,2) = perse(3,3) = 1;
-    perse(3,2) = -1.f/3;
+    Matrix perse = perspective();
+
     TGAImage africanDiffuse;
     africanDiffuse.read_tga_file("obj/african_head_diffuse.tga");
     africanDiffuse.flip_vertically();
@@ -138,25 +157,15 @@ int main(int argc, char** argv) {
     cout << "Nombre de face est de " << list.size() << endl;
     for (unsigned int i=0; i<list.size(); i++) {
         FloatElement pts[3], current[3], textures[3];
+        float intensity[3];
     	vector<IntElement> face = model->getFace(i);
     	for (int j=0; j<3; j++) {
 			FloatElement v0 = model->getElement(0,face[j].x);
-            FloatElement v1 = model->getElement(1,face[j].y);
+            FloatElement v2 = model->getElement(2,face[j].z);
             pts[j] = tmp.convMtoE(view*perse*tmp.convEtoM(v0));
-            textures[j] = FloatElement(v1.x, v1.y, v1.z);
-            current[j] = v0;
+            intensity[j] = v2.x*focus.x + v2.y*focus.y + v2.z*focus.z;
     	}
-    	/* calcule des vecteurs ab et ac */
-    	FloatElement ab = (current[1]-current[0]);
-    	FloatElement ac = (current[2]-current[0]);
-    	/* calcule du produit vectoriel de ab^ac */
-    	FloatElement normal = (ac)^(ab);
-    	normal.normaliser();
-
-    	float intensity = normal.x*focus.x + normal.y*focus.y + normal.z*focus.z;
-        if(intensity>0){
-            triangle(pts, textures, z_buffer, image, africanDiffuse, intensity);
-        }
+    	triangle(pts, textures, intensity, z_buffer, image, africanDiffuse);
     }
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
